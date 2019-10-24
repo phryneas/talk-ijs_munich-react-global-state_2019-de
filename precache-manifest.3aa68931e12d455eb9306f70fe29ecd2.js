40b474688172036ba11973da449939a19399397d
Lenz Weber <mail@lenzw.de>
2019-10-24 23:23:14 +0200

    Updates

---
+++ precache-manifest.3aa68931e12d455eb9306f70fe29ecd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c3aa024aebe2e60753e80508ef3349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd95dbbf126e5172d688",
+    "url": "/static/css/2.d416d538.chunk.css"
+  },
+  {
+    "revision": "cd95dbbf126e5172d688",
+    "url": "/static/js/2.e001f677.chunk.js"
+  },
+  {
+    "revision": "db524637b8035c479856",
+    "url": "/static/js/main.6956ad5c.chunk.js"
+  },
+  {
+    "revision": "a4556d860151418d31dc",
+    "url": "/static/js/runtime-main.900f2d3a.js"
+  },
+  {
+    "revision": "f5ca656dad4a4585ad48917c3908ff4d",
+    "url": "/static/media/Wollmilchsau.f5ca656d.png"
+  },
+  {
+    "revision": "b72db429c83a1ae6b90d67259731191f",
+    "url": "/static/media/mayflower_logo_transparent.b72db429.png"
+  }
+]);
